1d0680824846b8d77147cec24affdbb1c80accbe
GuillermoArispe <guillermo.arispe@gmail.com>
2015-04-07 11:18:57 -0300

    Se crea el proyecto

---
+++ app/src/androidTest/java/lp4/untref/edu/ar/imccalculator/ApplicationTest.java
@@ -0,0 +1,13 @@
+package lp4.untref.edu.ar.imccalculator;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
